149cbd7807b9d5d62b886f34e4cb4be76df9eaf1
ga111o <ga111o@proton.me>
2025-01-21 15:01:14 +0900

    comment: useAuth 설명 추가

--- jamovi/src/hooks/useAuth.jsx
+++ jamovi/src/hooks/useAuth.jsx
@@ -1,5 +1,9 @@
 import { useState, useEffect } from "react";
 
+/**
+ * 로그인 상태, 유저 이메일, 로딩 상태를 관리하는 훅
+ * @returns {Object} 로그인 상태(bool), 유저 이메일(@ 앞부분, str), 로딩 상태(bool), 로그인 함수, 로그아웃 함수
+ */
 export const useAuth = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userEmail, setUserEmail] = useState("");
@@ -13,6 +17,11 @@ export const useAuth = () => {
     }
   }, []);
 
+  /**
+   * 유저 이메일(이름대용?, @ 앞부분)을 서버에서 가져오는 함수
+   * @param {string} token - 로컬스토리지에 저장된 토큰
+   * @returns {Promise<void>} 성공 -> userEmail에 받아온 이메일(@ 앞부분) 넣음. 실패-> 로그아웃(토큰제거)
+   */
   const fetchUserInfo = async (token) => {
     try {
       const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/user`, {
