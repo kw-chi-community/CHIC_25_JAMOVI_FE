da253e56dd2373c7375472ca8bd673eb42c61e2b
junsy0901 <junsy0901@gmail.com>
2025-01-28 13:38:33 +0900

    remove: Remove side menubar

--- jamovi/src/components/Home.jsx
+++ jamovi/src/components/Home.jsx
@@ -1,41 +1,12 @@
 import React, { useEffect } from "react";
-import {
-  Drawer,
-  DrawerBody,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerOverlay,
-  DrawerContent,
-  DrawerCloseButton,
-  useDisclosure,
-  Button,
-} from "@chakra-ui/react";
-import Sidebar from "./Sidebar";
+import { useAuth } from "../hooks/useAuth";
 import DataTable from "./DataTable";
 import SelectOption from "./SelectOption";
 import Result from "./Result";
-import { Spinner, Text, VStack } from "@chakra-ui/react";
-import { useAuth } from "../hooks/useAuth";
 import LoadingSpinner from "./LoadingSpinner";
 
 const Home = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const [placement, setPlacement] = React.useState("right");
   const { isLoggedIn, isLoading } = useAuth();
-
-  useEffect(() => {
-    onOpen();
-    const handleMouseMove = (event) => {
-      if (window.innerWidth - event.clientX <= 10) {
-        onOpen();
-      }
-    };
-    window.addEventListener("mousemove", handleMouseMove);
-
-    return () => {
-      window.removeEventListener("mousemove", handleMouseMove);
-    };
-  }, [onOpen]);
 
   if (isLoading || !isLoggedIn) {
     return <LoadingSpinner />;
@@ -50,12 +21,6 @@ const Home = () => {
       <div className="w-1/2 min-h-full">
         <Result />
       </div>
-      <Drawer placement={placement} onClose={onClose} isOpen={isOpen}>
-        <DrawerOverlay />
-        <DrawerContent>
-          <Sidebar />
-        </DrawerContent>
-      </Drawer>
       <div></div>
     </div>
   );
